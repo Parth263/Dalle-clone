42a5f0a3ffdf630bb3a6dc5c27ea17daf3d37754
parth263 <prathmeshpatil623@gmail.com>
2024-03-06 18:18:07 +0530

    Navabr updated

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import {Loader, FormField, Card} from "../components"
+
+const Home = () => {
+  return <div>Home</div>;
+};
+
+export default Home;
